c10ab3ce916095fbcc2b3d872e3613d671c955e4
Fanghong <king1052369504@qq.com>
2021-10-17 05:18:54 +0800

    hot_fix第一次提交

--- src/main/java/com/fanghong/Git/GitTest.java
+++ src/main/java/com/fanghong/Git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello Git!!");
         System.out.println("hello Git!!222");
         System.out.println("hello Git!!333333");
+
+        System.out.println("hello hot_fix");//在hot_fix分支提交
     }
 }
